f5653a8db5a3ee44ef27ba2623c55e08e35afd66
nathaipti <ti.natan@ipti.org.br>
2023-05-10 10:27:45 -0300

    feat: add cores para o caminho percorrido por cada algoritmo

--- Maze-IA/constants.py
+++ Maze-IA/constants.py
@@ -3,6 +3,11 @@ white = (255, 255, 255)
 red = (255, 0, 0)
 black = (0, 0, 0)
 blue = (0, 0, 255)
+yellow = (255, 255, 0)
+green = (0, 255, 0)
+purple = (128,0,128)
+buscaAestrelaColor = (223, 198, 99)
+qLearningColor = (128, 193, 71)
 
 # definindo a posição inicial do quadrado
 x = 0
